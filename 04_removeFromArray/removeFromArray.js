9002b586c070715be534adc7e521d578ba1ac5c7
Chris Laska <chrislaska421@gmail.com>
2023-05-16 00:22:42 -0700

    04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...num) {
+    newarr = []
+    for (let i =0;i<arr.length;i++){
+       if (!num.includes(arr.at(i))) newarr.push(arr.at(i))
+    }
+    return newarr
 };
 
 // Do not edit below this line
